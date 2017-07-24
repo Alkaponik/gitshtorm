f311b1bb0d93f17fe4c2b165d218ee94019e8f55
turchin <turchin.magedoc@gmail.com>
2017-07-24 22:54:28 +0300

    All fikes

---
+++ cfg/survey.inc.php
@@ -0,0 +1,15 @@
+<?php
+	$survey_question = 'Как Вы обслуживаете свой авто?';
+
+	$survey_answers = array();
+	$survey_answers[] = 'Только на сервисном СТО для сохранения гарантии';
+	$survey_answers[] = 'Сам покупаю материалы, ТО прохожу на сервисном СТО';
+	$survey_answers[] = 'Доверяю только своим специалистам и ставлю печать о прохождении ТО для сохранения гарантии';
+	$survey_answers[] = 'Делаю ТО в гаражах и не доверяю сервисным СТО';
+
+	$survey_results = array();
+	$survey_results[] = "0";
+	$survey_results[] = "0";
+	$survey_results[] = "0";
+	$survey_results[] = "0";
+?>
